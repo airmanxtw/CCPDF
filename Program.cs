9e79f368f0ae869b3e3b952f152f9e5de63750f6
airmanxtw <airmanx@stust.edu.tw>
2023-04-24 13:32:49 +0800

    add resize image function

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ using System.CommandLine;
 
 var fileOption = new Option<FileInfo>(
             name: "--file",
-            description: "The PDF file to read");
+            description: "The file to read");
 
 var widthOption = new Option<int>(
             name: "--maxWidth",
@@ -20,14 +20,24 @@ var outputOption = new Option<FileInfo?>(
 
 var rootCommand = new RootCommand("CMPDF-based commands,in order to compress the pdf size");
 
-var compCommand = new Command("compress", "compress the file.")
+var compCommand = new Command("compress", "compress the pdf file")
     {
         fileOption,
         widthOption,
         outputOption,
     };
 
+var reimgCommand = new Command("resize", "resize the image file")
+    {
+        fileOption,
+            widthOption,
+            outputOption,
+    };
+
+
+
 rootCommand.AddCommand(compCommand);
+rootCommand.AddCommand(reimgCommand);
 
 compCommand.SetHandler((file, maxWidth, outputFile) =>
     {
@@ -50,6 +60,30 @@ compCommand.SetHandler((file, maxWidth, outputFile) =>
         }
     },
     fileOption, widthOption, outputOption);
+
+reimgCommand.SetHandler((file, maxWidth, outputFile) =>
+    {
+        CMPDF.PDF pdf = new CMPDF.PDF();
+        Console.WriteLine($"Processing {file.Name}, please wait....");
+        var fileBytes = File.ReadAllBytes(file.FullName);
+        var oriSize = fileBytes.Length;
+
+        var cbytes = pdf.ResizeImage(fileBytes, maxWidth,false);
+        var comSize = cbytes.Length;
+        if (outputFile != null)
+        {
+            File.WriteAllBytes(outputFile.FullName, cbytes);
+            Console.WriteLine($"Write {outputFile.Name} completed {efficiency(oriSize, comSize)}");
+        }
+        else
+        {
+            File.WriteAllBytes(file.FullName, cbytes);
+            Console.WriteLine($"Overwrite {file.Name} completed {efficiency(oriSize, comSize)}");
+        }
+    },
+    fileOption, widthOption, outputOption);
+
+
 
 static string efficiency(int ori, int com)
 {
